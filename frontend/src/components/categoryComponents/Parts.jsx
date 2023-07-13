abd5c9d2e8cbfff2af03bd6dc48ce48d760db10b
Omid-N <omid.c.nasi@gmail.com>
2023-07-13 10:58:59 -0700

    Complete structuring components for parts

--- frontend/src/components/categoryComponents/Parts.jsx
+++ frontend/src/components/categoryComponents/Parts.jsx
@@ -4,37 +4,98 @@ import { useLocation } from 'react-router-dom';
 const Parts = () => {
     const location = useLocation();
 
+    const [isAdded, setIsAdded] = useState(false)
+
+    // When ADD TO CART is clicked set state "setIsAdded" to true.
+    // Send a POST to the database table carts.
+    const addToCart = () => {}
+
+
+    // onClick={}
+    const button = (
+        <button 
+        class="px-6 py-1 transition ease-in duration-200 rounded-full hover:bg-gray-800 hover:text-white border-2 border-gray-900 focus:outline-none items-center mt-2" 
+        > 
+        ADD TO CART
+        </button>
+    )
+    
+    // ******************************
+
+    const quantityCount = (
+        <div class="flex items-center mb-2 mt-1">
+            <button
+            type="button"
+            class="w-10 py-1 mr-auto hover:bg-gray-300 rounded-full transition ease-in duration-200 border"
+            >
+            -
+            </button>
+
+            <input
+            type="number"
+            id="Quantity"
+            value="1"
+            class="w-16 py-1 border-transparent text-center [-moz-appearance:_textfield] sm:text-sm [&::-webkit-outer-spin-button]:m-0 [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:m-0 [&::-webkit-inner-spin-button]:appearance-none"
+            />
+
+            <button
+            type="button"
+            class="w-10 py-1 ml-auto hover:bg-gray-300 rounded-full transition ease-in duration-200 border"
+            >
+            +
+            </button>
+        </div>
+    )
+
+    // ******************************
+
     const [parts, setParts] = useState([]);
-
-    // Fetch `http://localhost:3001/api/inventory/category/${location.state.id}`
-    // Set state to an array of the data received.
-    // Map 
-
+    
     useEffect(() => {
-      fetch(`http://localhost:3001/api/inventory/category/${location.state.id}`)
+        fetch(`http://localhost:3001/api/inventory/category/${location.state.id}`)
         .then((data) => data.json())
-        // .then((jsonData) => console.log(location.state.id))
         .then((jsonData) => setParts(jsonData))
     }, [])
-
+    
     const part = parts.map((obj) => {
         return (
-            <li className='category border rounded flex'>
-                <div >
+            <div className='flex flex-row items-center border-2 w-full rounded-[16px]'>
+                {/* Name */}
+                <div className='ml-7 font-semibold'>
                     {obj.name}
                 </div>
 
-            </li>
+
+                <div className='ml-auto mr-2 flex flex-row items-center'>
+
+                    {/* Description */}
+                    <div class="relative hover-trigger mr-96">
+                        Description
+                        <div class="absolute bg-white border border-grey-100 px-4 py-2 hover-target rounded-[12px]">
+                            {obj.description}
+                        </div>
+                    </div>
+
+                
+                    {/* Price */}
+                    <div className='mr-20 flex flex-row'>
+                        ${obj.price}
+                    </div>
+
+                    {/* Buttons */}
+                    <div>
+                        {button}
+                        {quantityCount}
+                    </div>
+                </div>
+            </div>
         )
     })
 
     return (
         // Container
-        <div className='category-container ml-60 mr-60 mt-4 place-content-center flex flex-col items-center  space-y-1'>
-            <ul>
-                {part}
-            </ul>
-
+        <div className='flex flex-col items-center ml-60 mr-60 mt-4'>
+            {part}
         </div>
     )
 }
@@ -42,6 +103,12 @@ const Parts = () => {
 export default Parts;
 
 /*
+
+w-10 h-10 leading-10 text-gray-600 transition hover:opacity-75
+
+category border rounded flex max-w
+category-container ml-60 mr-60 mt-4 place-content-center flex flex-col items-center space-y-1 border-solid border-2 shadow-md rounded-lg
+
 category-container
 items-left
 
@@ -60,4 +127,8 @@ category-container
 place-content-center
 
 hover:bg-gray-50
+
+
+
+
 */
