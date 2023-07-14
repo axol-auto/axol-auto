f6c5f7b373104f89a08a181f5cff47a756fc0175
j9peters <29951738+j9peters@users.noreply.github.com>
2023-07-13 18:18:10 -0700

    building cart page

---
+++ frontend/src/containers/CartContainer.jsx
@@ -0,0 +1,37 @@
+import React, { useState } from 'react';
+import Item from '../components/Item';
+import NavBar from '../components/NavBar';
+
+const CartContainer = () => {
+
+    // need to get user ID somehow
+    const userId = null;
+
+    const [items, setItems] = useState([]);
+
+    // fetch data that is in cart, assign to state variable
+    fetch(`http://localhost:3000/api/cart/?userId=${userId}`)
+    .then((data) => data.json())
+    .then((data) => setItems(data))
+    .catch();
+
+
+    // 
+    return (
+      <div>
+        <NavBar></NavBar>
+        <div className=''>
+          {items.map((obj) => {
+            return (
+              <Item name={obj.name} price={obj.price} quantity={obj.quantity}></Item>
+            );
+          })}
+        </div>
+      </div>
+    );
+
+};
+
+
+
+export default CartContainer;
