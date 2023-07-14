ea2c347ff6ecd83ca592540d5f8fcf45c3d2f28b
Jun <junwenzhang@outlook.com>
2023-07-13 22:30:18 -0400

    Fixed stripe API call

--- backend/controllers/checkoutController.mjs
+++ backend/controllers/checkoutController.mjs
@@ -1,13 +1,15 @@
-import stripe from 'stripe';
+import Stripe from 'stripe';
 import dotenv from 'dotenv';
 import cartModel from '../models/Cart.mjs';
 
 dotenv.config();
 
+const stripe = new Stripe('sk_test_4eC39HqLyjWDarjtT1zdp7dc');
+
 const url = `http://localhost:3000`;
 
-const checkout = async (req, res, _next) => {
-  const items = cartModel.getCart(res.body);
+const checkout = async (req, res, next) => {
+  const items = await cartModel.getCart(req.body);
 
   const session = await stripe.checkout.sessions.create({
     line_items: items.map(item => ({
@@ -15,17 +17,42 @@ const checkout = async (req, res, _next) => {
         product_data: {
           name: item.name,
         },
+        unit_amount: item.price,
+        currency: 'usd',
       },
-      price: item.price,
       quantity: item.quantity,
     })),
-    mode: 'cards',
+    mode: 'payment',
+    payment_method_types: ['card'],
     success_url: url,
     cancel_url: url,
   });
 
-  res.redirect(303, session.url);
+  res.locals.redirect = session.url;
+
+  return next();
 };
+
+// const checkout = async (req, res, _next) => {
+//   const items = cartModel.getCart(res.body);
+
+//   const session = await stripe.checkout.sessions.create({
+//     line_items: items.map(item => ({
+//       price_data: {
+//         product_data: {
+//           name: item.name,
+//         },
+//       },
+//       price: item.price,
+//       quantity: item.quantity,
+//     })),
+//     mode: 'cards',
+//     success_url: url,
+//     cancel_url: url,
+//   });
+
+//   res.redirect(303, session.url);
+// };
 
 export default {
   checkout,
--- backend/routes/checkoutRouter.mjs
+++ backend/routes/checkoutRouter.mjs
@@ -3,6 +3,10 @@ import checkoutController from '../controllers/checkoutController.mjs';
 
 const router = Router();
 
-router.post('/', checkoutController.checkout);
+router.post('/', checkoutController.checkout, (req, res) => {
+  res.send(res.locals.redirect);
+});
+
+// router.post('/', (req, res) => res.send('hi'));
 
 export default router;
